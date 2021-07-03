f71c14e808c358d802d70070c2ad7e37231ed158
Nikodem Kosznik <ghostsoldier99@gmail.com>
2021-07-03 14:54:27 +0200

    Getting shapes!!!!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,53 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    int wyborGracza;
+
+    wyborGracza = menu();
+
+    switch (wyborGracza){
+        case 1:
+            newGame();
+            break;
+        case 2:
+            System.out.println("Gra to symulator farmy - od zera do bohatera");
+            System.out.println("Zaczynając od podstaw kupujemy więcej ziemi, zarabiamy, a także i tracimy");
+            System.out.println("Have fun!!!");
+            System.out.println("Napisał: Nikodem Kosznik 53535");
+            break;
+        case 3:
+            System.exit(0);
+            break;
     }
+
+
+    }
+
+    private static void newGame() {
+        new Game();
+
+
+
+    }
+
+
+    public static int menu(){
+        int wybor;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("witam w Farm Lords");
+        System.out.println("---------------------------");
+        System.out.println("1. Nowa gra");
+        System.out.println("2. Pomoc");
+        System.out.println("3. Wyjście");
+
+        wybor = input.nextInt();
+
+        return wybor;
+    }
+
 }
---
+++ src/com/company/plot/Ziemia.java
@@ -0,0 +1,6 @@
+package com.company.plot;
+
+public class Ziemia {
+
+
+}
